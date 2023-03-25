3a87bf31e893cfc333b7f81b62be40ca8e123f05
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-25 14:52:35 +0800

    Update index.py

--- index.py
+++ index.py
@@ -55,5 +55,4 @@ def handle_text_message(event):
 
 # 啟動 Flask
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    app.run()
+    app.run(debug=True)
