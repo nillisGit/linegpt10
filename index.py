b6da85e145e9cfe30d45a5cd9aeac48452ca9719
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 14:30:53 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,8 +10,8 @@ import openai
 app = Flask(__name__)
 
 
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
+#line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+#handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 #openai.api_key = os.getenv('OpenAIkey')
 model_engine = "davinci"
 
