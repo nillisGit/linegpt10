b3cceb5ffbc369c32cdffc87e23557b7cc21fc88
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 15:19:19 +0800

    Update index.py

--- index.py
+++ index.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 
 mm= os.getenv('CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi('Bearer ' + mm)
-handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
+#handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 #openai.api_key = os.getenv('OpenAIkey')
 model_engine = "davinci"
 
 @app.route('/')
 def home():
-    return "1531" #os.getenv("CHANNEL_SECRET")
+    return os.getenv("CHANNEL_SECRET")
 
 # 啟動 Flask
 if __name__ == "__main__":
