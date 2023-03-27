a089d20b3cb1f069b70efdecbc82e798029661ff
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 13:43:18 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,58 @@
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
 import openai
+#from dotenv import load_dotenv
 
-
+#load_dotenv()
 app = Flask(__name__)
+
+
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+openai.api_key = os.getenv('OpenAIkey')
+model_engine = "davinci"
+
+def generate_response(user_message):
+    prompt = (f"The following is a conversation with a user about {user_message}. The user says:") # 設定 GPT-3 問題
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = response.choices[0].text.strip() # 獲得 GPT-3 回答
+    return message
 
 
 @app.route('/')
 def home():
-    return os.getenv('LINE_CHANNEL_SECRET', None)
+    return "Home base"
 
 
+# 設定 Line bot 的 Webhook
+@app.route("/Webhook", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+# 設定 Line bot 回應
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    user_message = event.message.text
+    reply_message = generate_response(user_message) # 使用 GPT-3 API 產生回應
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
+# 啟動 Flask
 if __name__ == "__main__":
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     app.run()
