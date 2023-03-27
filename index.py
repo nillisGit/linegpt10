101860ad173b53f25557e431d788b98effb9228d
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 10:24:51 +0800

    Update index.py

--- index.py
+++ index.py
@@ -7,14 +7,9 @@ import openai
 
 
 app = Flask(__name__)
-k1 = os.getenv('token')
-k2 = os.getenv('cs')
-k3 = os.getenv('aikey')
-
-line_bot_api = LineBotApi(k1)
-handler = WebhookHandler(k2)
-
-openai.api_key = k3
+line_bot_api = LineBotApi(os.getenv("token"))
+handler = WebhookHandler(os.getenv("cs"))
+openai.api_key = os.getenv("aikey")
 model_engine = "davinci"
 
 
@@ -37,7 +32,7 @@ def home():
     return 'H3small!'
 
 
-@app.route("/Webhook", methods=["POST"])
+@app.route("/webhook", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
