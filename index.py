f3994a20922315f1eb441bddc56791476b127865
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 13:55:54 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,9 +10,9 @@ import openai
 app = Flask(__name__)
 
 
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+#line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-openai.api_key = os.getenv('OpenAIkey')
+#openai.api_key = os.getenv('OpenAIkey')
 model_engine = "davinci"
 
 def generate_response(user_message):
