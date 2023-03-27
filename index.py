03dc1b4217c2a9b6701210c83b618bcc25ec7e66
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-27 15:57:37 +0800

    Update index.py

--- index.py
+++ index.py
@@ -16,11 +16,39 @@ hh = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 openai.api_key = os.getenv('OpenAIkey')
 model_engine = "davinci"
 
+def generate_response(user_message):
+    prompt = (f"The following is a conversation with a user about {user_message}. The user says:") 
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = response.choices[0].text.strip() 
+    return message
+
 @app.route('/')
 def home():
-    return '99afe15305'+os.getenv("CHANNEL_SECRET")
+    return 'H3small!'
 
-# 啟動 Flask
+
+@app.route("/webhook", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    user_message = event.message.text
+    reply_message = generate_response(user_message)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     app.run()
