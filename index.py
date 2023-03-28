540fce18af3be5242e2953dbdf27925ba3eba425
nillisGit <54298542+nillisGit@users.noreply.github.com>
2023-03-28 16:13:44 +0800

    Update index.py

--- index.py
+++ index.py
@@ -8,29 +8,14 @@ import openai
 app = Flask(__name__)
 
 
-mm= os.getenv('CHANNEL_ACCESS_TOKEN')
-line_bot_api = LineBotApi(mm)
+
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 hh = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 openai.api_key = os.getenv('OpenAIkey')
-
-
-def generate_response(user_message):
-    prompt = (f"The following is a conversation with a user about {user_message}. The user says:") 
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    message = response.choices[0].text.strip() 
-    return message
 
 @app.route('/')
 def home():
     return 'H3small!'
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -42,13 +27,20 @@ def webhook():
         abort(400)
     return 'OK'
     
-
-
 @hh.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     user_message = event.message.text
-    reply_message_text = generate_response(user_message)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message_text))
+    prompt = (f"Message==> {user_message}. GPT replys:") 
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    gptreply = response.choices[0].text.strip()     
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=gptreply))
 
 if __name__ == "__main__":
     app.run()
